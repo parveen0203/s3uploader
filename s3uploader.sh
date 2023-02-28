5ff39292aee40d404607caf242b7b0481d8d964d
parveen0203 <48429431+parveen0203@users.noreply.github.com>
2023-02-28 16:30:45 -0500

    Adding comments to the script

--- s3uploader.sh
+++ s3uploader.sh
@@ -6,6 +6,7 @@ export secret_access_key=“AbG7TGGV74H5ydIlAWP0EEIp6GD6RvbqMuXBB/y+”
 bucket_name=$1
 file_name=$2
 
+#Verfying if myfile.txt already exist or not
 aws s3api head-object --bucket $bucket_name --key $file_name || NOT_EXIST=true
 if [ $NOT_EXIST ]; then
   echo "File does not exist."
@@ -13,7 +14,7 @@ if [ $NOT_EXIST ]; then
   exit 0
 fi
 
-#Downloading from S3 bucket and comparing with previously uploaded file
+#Downloading from S3 bucket and comparing with previously uploaded file.If content changed then upload it again with new content
 aws s3api get-object --bucket $bucket_name --key $file_name current.txt
 if diff $file_name current.txt; then
    echo "no file change"
